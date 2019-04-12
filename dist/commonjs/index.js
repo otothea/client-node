a94d618781387f0a01bccd21756554558f9e4f41
Oscar Armstrong <oscar@otothea.com>
2019-04-12 18:37:50 -0500

    add dist so i can use directly from github

---
+++ dist/commonjs/index.js
@@ -0,0 +1,18 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./providers/OAuth"));
+__export(require("./providers/Provider"));
+__export(require("./services/Channel"));
+__export(require("./services/Chat"));
+__export(require("./services/Clips"));
+__export(require("./services/Game"));
+__export(require("./services/Service"));
+__export(require("./ws/Socket"));
+__export(require("./ws/Reply"));
+__export(require("./Client"));
+__export(require("./errors"));
+__export(require("./RequestRunner"));
+//# sourceMappingURL=index.js.map
---
+++ dist/module/index.js
@@ -0,0 +1,13 @@
+export * from './providers/OAuth';
+export * from './providers/Provider';
+export * from './services/Channel';
+export * from './services/Chat';
+export * from './services/Clips';
+export * from './services/Game';
+export * from './services/Service';
+export * from './ws/Socket';
+export * from './ws/Reply';
+export * from './Client';
+export * from './errors';
+export * from './RequestRunner';
+//# sourceMappingURL=index.js.map
